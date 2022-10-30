28bcbcc9ef3b944afe4bbc2b5162f0ce3f073fad
jdp012 <jayqatariya@gmail.com>
2022-10-30 14:40:26 -0400

    upadted assign2

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports =
+ {
+"URI": "mongodb+srv://jaykatariya:jay123@cluster0.t44umqy.mongodb.net/book_store?retryWrites=true&w=majority"
+//"URI":"mongodb://127.0.0.1:27017/book_store"
+}
